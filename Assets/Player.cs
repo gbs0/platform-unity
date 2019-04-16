cdeaefbf48cd0666654760e93fa915d5bc2298f7
gabriel schiavo <gabriel@lewagon.org>
2019-04-16 20:14:06 -0300

    rigid body fix

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Rigidbody2D rb;
+
+
+    Vector2 mov = rb.velocity;
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<>(Type type);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            Transform.Translate(0.1f, 0, 0);                        // Add new translates for x axis
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            Transform.Translate(-0.1f, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            Transform.Translate(0, 0.1f, 0);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            Transform.Translate(0, -0.1f, 0);
+        }
+
     }
 }
