cd67a6e6c35f96c7c4feccc90beab966c7076244
gabriel schiavo <gabriel@lewagon.org>
2019-04-16 21:29:19 -0300

    add ground check by layer

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,11 +6,13 @@ public class Player : MonoBehaviour
 {
     Rigidbody2D rb;
 
+    BoxCollider2D groundCheck;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        groundCheck = transform.find("groundCheck").GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -33,7 +35,10 @@ public class Player : MonoBehaviour
       // }
 
       mov.x = Input.GetAxis("Horizontal") * 5;
-      mov.y = Input.GetAxis("Vertical") * 5; 
+      
+      if (Input.GetAxis("Jump") > 0) {
+        mov.y = 10;
+      } 
 
       rb.velocity = mov;
     }
